2a1137cf5b999058f1cefce6d2c1b85fb47a8626
Sai-niruthi008 <charan.niruthi@gmail.com>
2025-07-21 18:46:04 +0530

    Update UI_SWB.py

--- UI_SWB.py
+++ UI_SWB.py
@@ -157,7 +157,7 @@ def create_timeseries_chart(df, lat, lon):
 # ======================================================================================
 # 4. MAIN APP INTERFACE
 # ======================================================================================
-st.title = ( "🌾 Irrisnap Pixel wise Irrigation Advisories Dashboard" )
+st.title("🌾 Irrisnap Pixel wise Irrigation Advisories Dashboard")
 st.markdown(
     "Select a variable and date from the sidebar to view the spatial map. "
     "**Click on any pixel** on the map to generate a detailed time-series water balance chart for that location."
